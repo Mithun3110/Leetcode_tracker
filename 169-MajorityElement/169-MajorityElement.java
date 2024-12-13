bc4af6e673af8c3ba61c00cbb0f522853601b217
Mithun <102860039+Mithun3110@users.noreply.github.com>
2024-12-12 19:36:21 -0500

    Update file 12/12/2024, 7:36:21 pm

--- 169-MajorityElement/169-MajorityElement.java
+++ 169-MajorityElement/169-MajorityElement.java
@@ -1,19 +1,7 @@
 class Solution {
     public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
         int n = nums.length;
-        int res = 0;
-        for(int i=0;i<n;i++){
-            int count =0;
-            for(int j=i;j<n;j++){
-                if(nums[i]==nums[j]){
-                    count++;
-                }
-            }
-            if(count > n/2){
-                res = nums[i];
-                break;
-            }
-        }
-        return res;
+        return nums[n/2];
     }
 }
